ffcd34242bfcc2b448608d0cf2dc94685892895d
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-04-19 22:42:22 +0100

    Add support for the subunit v2 binary protocol
    New Test::Subunit::V2 module implements the v2 packet format (signature,
    flags, variable-length numbers, timestamps, tags, file content, routing
    code, CRC-32). parse_results auto-detects v2 by the 0xB3 signature byte
    and dispatches, keeping the existing msg_ops interface.

--- lib/Test/Subunit.pm
+++ lib/Test/Subunit.pm
@@ -35,7 +35,35 @@ sub parse_results($$$)
 	my $unexpected_err = 0;
 	my $open_tests = [];
 
-	while(<$fh>) {
+	# Peek at the first byte: v2 packets start with 0xB3, a byte that
+	# cannot occur at a line start in the line-oriented v1 protocol.
+	my $first = '';
+	my $peeked = read($fh, $first, 1);
+	return 0 if !$peeked;
+	if (ord($first) == 0xB3) {
+		require Test::Subunit::V2;
+		my $rest;
+		{ local $/; $rest = <$fh>; }
+		$rest = '' unless defined $rest;
+		my $combined = $first . $rest;
+		open my $mem, '<', \$combined
+			or die "cannot reopen memory stream: $!";
+		return Test::Subunit::V2::parse_stream(
+			$msg_ops, $statistics, $mem);
+	}
+
+	my $pending_line = $first;
+	while (1) {
+		my $rest_of_line = <$fh>;
+		if (defined $rest_of_line) {
+			$_ = $pending_line . $rest_of_line;
+			$pending_line = '';
+		} elsif (length $pending_line) {
+			$_ = $pending_line;
+			$pending_line = '';
+		} else {
+			last;
+		}
 		if (/^test: (.+)\n/) {
 			$msg_ops->control_msg($_);
 			$msg_ops->start_test($1);
